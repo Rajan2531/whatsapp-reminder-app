f05ac61f3f4d31fc0111204d2f277c71f7abada1
rajan chouhan <rajan.chouhan25@gmail.com>
2024-08-30 14:31:20 +0530

    fully working

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser")
 const getCompletion = require("./openai")
 dotenv.config({path: "./config.env"})
 app.use(express.json());
-
+app.use(bodyParser.json());
 app.get("/", (req, res)=> {
     console.log(process.env.WHATSAPP_API)
     res.status(200).json({
@@ -14,9 +15,10 @@ app.get("/", (req, res)=> {
 })
 
 app.get("/webhook", (req, res)=> {
+    console.log(req.query);
     const mode = req.query["hub.mode"];
     const challenge = req.query["hub.challenge"];
-    const token = req.query["hub.token"];
+    const token = req.query["hub.verify_token"];
     if(mode === "subscribe" && token == process.env.WHATSAPP_WEBHOOK_VERIFY_TOKEN) {
         res.status(200).send(challenge);
     } else {
@@ -27,9 +29,11 @@ app.get("/webhook", (req, res)=> {
 
 
 app.post("/webhook", async (req, res)=> {
-    const message = req.body.entry?.changes[0]?.value?.messages?.[0];
+    
+    const message = req.body.entry[0]?.changes[0]?.value?.messages?.[0];
+    console.log(message);
     if(message?.type === "text") {
-        const business_phone_number_id = req.body.entry?.changes[0]?.value?.metadata?.phone_number_id;
+        const business_phone_number_id = req.body.entry[0]?.changes[0]?.value?.metadata?.phone_number_id;
 
         // sending message as reply
         await axios({
@@ -42,13 +46,30 @@ app.post("/webhook", async (req, res)=> {
             data:{
                 "messaging_product": "whatsapp",
                 "to":message.from,
-                "text": {body: "Echo: " + message.text.body},
-                "context": {
-                    "message_id": message_id
-                }
+                "text": {body:  message.text.body}
+                
+               
+                
+            }
+        })
+        await axios({
+            url:` https://graph.facebook.com/v20.0/${business_phone_number_id}/messages `,
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.WHATSAPP_API}`,
+                "Content-Type": "application/json"
+            },
+            data:{
+                "messaging_product": "whatsapp",
+                "status":"read",
+                "message_id": message.id
+               
+                
             }
         })
     }
+    res.sendStatus(200).end();
 })
+       
 
 module.exports = app;
